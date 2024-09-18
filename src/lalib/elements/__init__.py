3cfc0db136b652d7b4515a7ecc78ca4b8db8dcd8
Alexander Hess <alexander@webartifex.biz>
2024-09-18 18:29:40 +0200

    Organize top-level imports for `lalib.elements`
    - make `GF2`, `one`, and `zero`, defined in the `lalib.elements.gf2`
      module, available as top-level imports in the `lalib.elements`
      sub-package via `from lalib.elements import *`
    - provide some code snippets in the sub-package's docstring
    - test the star import

--- src/lalib/elements/__init__.py
+++ src/lalib/elements/__init__.py
@@ -1,1 +1,34 @@
-"""Various elements of various fields."""
+"""Various elements of various fields.
+
+Import the objects like so:
+
+>>> from lalib.elements import *
+
+Then, use them:
+
+>>> one + zero
+one
+
+>>> GF2(0)
+zero
+>>> GF2(42)
+Traceback (most recent call last):
+...
+ValueError: ...
+>>> GF2(42, strict=False)
+one
+"""
+
+from lalib.elements import gf2
+
+
+GF2, one, zero = gf2.GF2, gf2.one, gf2.zero
+
+del gf2
+
+
+__all__ = (
+    "GF2",
+    "one",
+    "zero",
+)
--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -14,6 +14,7 @@ import xdoctest
     "module",
     [
         "lalib",
+        "lalib.elements",
         "lalib.elements.gf2",
     ],
 )
